dff6bc6f52d0103610802a28ae768a2b47c72721
Cheon1219 <j1219j1@naver.com>
2025-02-20 15:34:39 +0900

    Add, Update
    공격 충돌판정과 아이템 드롭을 제외한 기본적인 틀 완성

---
+++ Source/EdmundPrj/Public/Monster/MeleeMonster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/BaseMonster.h"
+#include "MeleeMonster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDMUNDPRJ_API AMeleeMonster : public ABaseMonster
+{
+	GENERATED_BODY()
+	
+};
