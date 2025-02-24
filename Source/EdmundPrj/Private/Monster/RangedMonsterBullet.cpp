5e46220122c4be5f79bc73f2a426b0f252712035
Cheon1219 <j1219j1@naver.com>
2025-02-24 12:24:18 +0900

    Add: RangedMonsterBullet
    add

--- Source/EdmundPrj/Private/Monster/RangedMonsterBullet.cpp
+++ Source/EdmundPrj/Private/Monster/RangedMonsterBullet.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Monster/MonsterBullet.h"
+#include "Monster/RangedMonsterBullet.h"
 
 // Sets default values
-AMonsterBullet::AMonsterBullet()
+ARangedMonsterBullet::ARangedMonsterBullet()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,14 +12,14 @@ AMonsterBullet::AMonsterBullet()
 }
 
 // Called when the game starts or when spawned
-void AMonsterBullet::BeginPlay()
+void ARangedMonsterBullet::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void AMonsterBullet::Tick(float DeltaTime)
+void ARangedMonsterBullet::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
